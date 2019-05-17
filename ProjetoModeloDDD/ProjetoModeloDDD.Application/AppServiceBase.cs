bf1431c15c5d1fc8090d71672d051e1ca3a56601
Anderson Oliveira Magalhaes <andoliveira4@gmail.com>
2019-05-17 01:18:19 -0300

    First commit

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Application/AppServiceBase.cs
@@ -0,0 +1,51 @@
+﻿using ProjetoModeloDDD.Application.Interface;
+using ProjetoModeloDDD.Domain.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoModeloDDD.Application
+{
+    public class AppServiceBase<TEntity> : IDisposable, IAppServiceBase<TEntity> where TEntity : class
+    {
+        private readonly IServiceBase<TEntity> _servicebase;
+
+        public AppServiceBase(IServiceBase<TEntity> servicebase)
+        {
+            _servicebase = servicebase;
+        }
+
+        public void Add(TEntity obj)
+        {
+            _servicebase.Add(obj);
+        }        
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _servicebase.GetAll();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _servicebase.GetById(id);
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _servicebase.Remove(obj);
+        }
+
+        public void Update(TEntity obj)
+        {
+            _servicebase.Update(obj);
+        }
+
+        public void Dispose()
+        {
+            _servicebase.Dispose();
+        }
+
+    }
+}
